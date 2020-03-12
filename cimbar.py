d46c437c767ea1591559128911b2bd421eb94aed
sz <sz@galacticicecube.com>
2020-03-12 16:54:03 -0500

    Docopt and such

--- cimbar.py
+++ cimbar.py
@@ -1,18 +1,39 @@
-'''
-color-iconographic-matrix barcode
-'''
+#!/usr/bin/python3
 
+"""color-iconographic-matrix barcode
+
+Usage:
+  ./cimbar.py [<src_image> | --src_image=<filename>] [<dst_data> | --dst_data=<filename>]
+  ./cimbar.py --encode [<src_data> | --src_data=<filename>] [<dst_image> | --dst_image=<filename>]
+  ./cimbar.py (-h | --help)
+
+Examples:
+  python -m cimbar --encode myfile.txt cimb-code.png
+  python -m cimbar cimb-code.png myfile.txt
+
+Options:
+  -h --help                        Show this help.
+  --version                        Show version.
+  --dst_data=<filename>            For decoding. Where to store decoded data.
+  --dst_image=<filename>           For encoding. Where to store encoded image.
+  --src_data=<filename>            For encoding. Data to encode.
+  --src_image=<filename>           For decoding. Image to try to decode
+"""
+
+import bitstring
 import imagehash
-import sys
-
 from bitstring import Bits, BitStream
+from docopt import docopt
 from PIL import Image
 
 
 CELL_SIZE = 8
+CELL_SPACING = CELL_SIZE
+CELL_DIMENSIONS = 128
+MAX_ENCODING = 16384
 
 
-class Translator:
+class CimbTranslator:
     def __init__(self):
         self.hashes = {}
         for i in range(32):
@@ -39,42 +60,71 @@ class Translator:
         return self.get_best_fit(cell_hash)
 
 
-class OutputStream:
-    def __init__(self, output_file):
+class bit_file:
+    def __init__(self, filename, bits_per_op, mode='read'):
+        if mode not in ['read', 'write']:
+            raise Exception('bad bit_file mode. Try "read" or "write"')
+        self.mode = 'wb' if mode == 'write' else 'rb'
+
+        self.f = open(filename, self.mode)
+        self.bits_per_op = bits_per_op
         self.stream = BitStream()
-        self.output_file = output_file
+        if mode == 'read':
+            self.stream.append(Bits(bytes=self.f.read(MAX_ENCODING)))
 
     def __enter__(self):
         return self
 
     def __exit__(self, type, value, traceback):
-        self.save()
+        if self.mode == 'wb':
+            self.save()
+        with self.f:  # close file
+            pass
 
     def write(self, bits):
-        b1 = Bits(uint=bits, length=5)
+        b1 = Bits(uint=bits, length=self.bits_per_op)
         self.stream.append(b1)
 
+    def read(self):
+        try:
+            bits = self.stream.read(f'uint:{self.bits_per_op}')
+        except bitstring.ReadError:
+            try:
+                bits = self.stream.read('uint')
+            except bitstring.InterpretError:
+                bits = 0
+        return bits
+
     def save(self):
-        with open(self.output_file, 'wb') as f:
-            self.stream.tofile(f)
-        print('wrote {}'.format(self.output_file))
+        self.stream.tofile(self.f)
+
+
+def decode(src_image, outfile):
+    img = Image.open(src_image)
+    ct = CimbTranslator()
+
+    cols = CELL_DIMENSIONS
+    cells = CELL_DIMENSIONS * cols
+
+    with bit_file(outfile, bits_per_op=5, mode='write') as f:
+        for i in range(cells):
+            x = (i % cols) * CELL_SPACING
+            y = (i // cols) * CELL_SPACING
+            img_cell = img.crop((x, y, x + CELL_SIZE, y + CELL_SIZE))
+            bits = ct.decode(img_cell)
+            f.write(bits)
 
 
 def main():
-    src_image = sys.argv[1]
-    img = Image.open(src_image)
-    tra = Translator()
+    args = docopt(__doc__, version='CIMBar 0.0.1')
+    if args['--encode']:
+        src_data = args['<src_data>'] or args['--src_data']
+        dst_image = args['<dst_image>'] or args['--dst_image']
+        return
 
-    cols = 128
-    cells = 128 * cols
-
-    with OutputStream('/tmp/myfile.txt') as os:
-        for i in range(cells):
-            x = (i % cols) * CELL_SIZE
-            y = (i // cols) * CELL_SIZE
-            img_cell = img.crop((x, y, x + CELL_SIZE, y + CELL_SIZE))
-            bits = tra.decode(img_cell)
-            os.write(bits)
+    src_image = args['<src_image>'] or args['--src_image']
+    dst_data = args['<dst_data>'] or args['--dst_data']
+    decode(src_image, dst_data)
 
 
 if __name__ == '__main__':
