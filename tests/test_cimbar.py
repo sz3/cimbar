3a2adfcd995a36b060d1c045b544b75688067dd9
sz <sz@galacticicecube.com>
2020-05-25 20:20:36 -0500

    Beginnings of a more interesting test, maybe

--- tests/test_cimbar.py
+++ tests/test_cimbar.py
@@ -2,12 +2,25 @@ from os import path
 from tempfile import TemporaryDirectory
 from unittest import TestCase
 
+import cv2
+import numpy
+
 from cimbar.cimbar import encode, decode, BITS_PER_OP
 from cimbar.encode.rss import reed_solomon_stream
 from cimbar.grader import evaluate as evaluate_grader
 
 
 CIMBAR_ROOT = path.abspath(path.join(path.dirname(path.realpath(__file__)), '..'))
+
+
+def _warp1(src_image, dst_image):
+    img = cv2.imread(src_image)
+    img = cv2.blur(img, (2,2))
+    input_pts = [(0, 0), (0, 1023), (1023, 0), (1023, 1023)]
+    output_pts = [(21, 212), (115, 943), (854, 198), (795, 942)]
+    transformer = cv2.getPerspectiveTransform(numpy.float32(input_pts), numpy.float32(output_pts))
+    img = cv2.warpPerspective(img, transformer, (1000, 1000))
+    cv2.imwrite(dst_image, img)
 
 
 class CimbarTest(TestCase):
@@ -69,4 +82,12 @@ class CimbarTest(TestCase):
 
         out_no_ecc = self._temp_path('outfile_no_ecc.txt')
         decode(self.encoded_file, out_no_ecc, dark=True, ecc=0)
+        self.validate_grader(out_no_ecc, 200)
+
+    def test_decode_perspective(self):
+        skewed_image = self._temp_path('skewed.jpg')
+        _warp1(self.encoded_file, skewed_image)
+
+        out_no_ecc = self._temp_path('outfile_no_ecc.txt')
+        decode(skewed_image, out_no_ecc, dark=True, ecc=0)
         self.validate_grader(out_no_ecc, 200)
