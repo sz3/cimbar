bcae6c1fcb8ff055a6651ed80cd2f3aea339946e
sz <sz@recv.cc>
2023-06-11 07:33:25 -0500

    Possibly interesting grid configurations
    Also, *maybe* should switch to ecc_block_size=162 for 5x5, it lends
    itself to more ecc flexibility.

--- cimbar/conf.py
+++ cimbar/conf.py
@@ -29,8 +29,8 @@ class sq5x5:
     CELL_SPACING_X = CELL_SIZE + 1
     CELL_DIM_X = 162
     CELLS_OFFSET = 9
-    ECC = 40
-    ECC_BLOCK_SIZE = 216
+    ECC = 40  # 32?
+    ECC_BLOCK_SIZE = 216  # 162?
     INTERLEAVE_PARTITIONS = 2
     FOUNTAIN_BLOCKS = 10
 
@@ -42,14 +42,56 @@ class sq5x5:
 
 
 class sq5x6:
-    TOTAL_SIZE = 1006
+    TOTAL_SIZE = 966
     BITS_PER_SYMBOL = 2
     BITS_PER_COLOR = 2
     CELL_SIZE = 5
     CELL_SPACING_X = CELL_SIZE
     CELL_SPACING_Y = CELL_SIZE + 1
-    CELL_DIM_Y = 165
-    CELL_DIM_X = 198
+    CELL_DIM_Y = 158
+    CELL_DIM_X = 190
+    CELLS_OFFSET = 9
+    ECC = 31
+    ECC_BLOCK_SIZE = 161
+    INTERLEAVE_PARTITIONS = 23  # or just leave ecc locked, and do 2,10?
+    FOUNTAIN_BLOCKS = 23
+
+    INTERLEAVE_BLOCKS = ECC_BLOCK_SIZE
+    MARKER_SIZE_X = round(54 / CELL_SPACING_X)
+    MARKER_SIZE_Y = round(54 / CELL_SPACING_Y)  # 6 or 9, probably
+
+
+# this one is very flexible, probably good for experimenting with
+class sq5x6alt:
+    TOTAL_SIZE = 958
+    BITS_PER_SYMBOL = 2
+    BITS_PER_COLOR = 2
+    CELL_SIZE = 5
+    CELL_SPACING_X = CELL_SIZE
+    CELL_SPACING_Y = CELL_SIZE + 1
+    CELL_DIM_Y = 157
+    CELL_DIM_X = 188
+    CELLS_OFFSET = 9
+    ECC = 32
+    ECC_BLOCK_SIZE = 182
+    INTERLEAVE_PARTITIONS = 2
+    FOUNTAIN_BLOCKS = 10
+
+    INTERLEAVE_BLOCKS = ECC_BLOCK_SIZE
+    MARKER_SIZE_X = round(54 / CELL_SPACING_X)
+    MARKER_SIZE_Y = round(54 / CELL_SPACING_Y)  # 6 or 9, probably
+
+
+# swing for the fences
+class sq5x6beeg:
+    TOTAL_SIZE = 1051
+    BITS_PER_SYMBOL = 2
+    BITS_PER_COLOR = 2
+    CELL_SIZE = 5
+    CELL_SPACING_X = CELL_SIZE
+    CELL_SPACING_Y = CELL_SIZE + 1
+    CELL_DIM_Y = 172
+    CELL_DIM_X = 207
     CELLS_OFFSET = 9
     ECC = 33
     ECC_BLOCK_SIZE = 163
